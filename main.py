7b5b8795d73bc4a912dc7e410c14d399784a4af6
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-03-20 05:32:50 +0500

    Added Error Handling

--- main.py
+++ main.py
@@ -3,20 +3,18 @@ from file_loader import VideoFileLoader
 from favorites_manager import FavoritesManager
 if __name__ == "__main__":
     folder_path_string = input("Enter folder path(s) separated by comma: ").strip()
-    # video_files = VideoFileLoader(csv_folder="Files/")
     vf_loader = VideoFileLoader()
-    if folder_path_string == "play favs":
-        favs = FavoritesManager()
-        video_files = favs.get_favorites()
-    else:
-        video_files = vf_loader.start_here(folder_path_string)
-    # testing_csv_path = video_files.check_folders_path(folder_paths)
-    # if testing_csv_path:
-    #     print("Testing CSV Path:", testing_csv_path)
-    # video_files = vf_loader.get_videos_from_paths(folder_paths=folder_paths)
-    # video_files = vf_loader.get_videos_from_csv(folder_paths)
-
+    try:
+        if folder_path_string == "play favs":
+            favs = FavoritesManager()
+            video_files = favs.get_favorites()
+        else:
+            video_files = vf_loader.start_here(folder_path_string)
+    except Exception as e:
+        print(f"An Error Occurred: {e}")
+        video_files = vf_loader.get_videos_from_paths(folder_paths=folder_path_string.split(","))
     if video_files:
+        print(f"Total Videos Found: {len(video_files)}")
         app = MediaPlayerApp(video_files)
         app.update_video_progress()
         app.mainloop()
