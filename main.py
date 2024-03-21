9f4c71eebcdb628a4c488be4c62a8bbe9734e3d0
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-03-21 22:39:53 +0500

    Version 2.1.2: Added Printing of Fav Paths From fav_paths file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from videoplayer import MediaPlayerApp
 from file_loader import VideoFileLoader
 from favorites_manager import FavoritesManager
+
+FAV_PATHS = "Files/fav_paths.txt"
+
 if __name__ == "__main__":
     folder_path_string = input("Enter folder path(s) separated by comma: ").strip()
     vf_loader = VideoFileLoader()
@@ -8,6 +11,12 @@ if __name__ == "__main__":
         if folder_path_string == "play favs":
             favs = FavoritesManager()
             video_files = favs.get_favorites()
+        elif folder_path_string == "show favs":
+            with open("Files/fav_paths.txt", "r", encoding="utf-8") as file:
+                reader = file.readlines()
+                # print(reader)
+            folder_path_string = input("Enter folder path(s) or Your Favs: ").strip()
+            video_files = vf_loader.start_here(folder_path_string)
         else:
             video_files = vf_loader.start_here(folder_path_string)
     except Exception as e:
@@ -15,7 +24,7 @@ if __name__ == "__main__":
         video_files = vf_loader.get_videos_from_paths(folder_paths=folder_path_string.split(","))
     if video_files:
         print(f"Total Videos Found: {len(video_files)}")
-        app = MediaPlayerApp(video_files)
+        app = MediaPlayerApp(video_files, random_select=True)
         app.update_video_progress()
         app.mainloop()
     else:
