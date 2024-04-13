760fd9270f7a31641e130a6cb1ab26e0a9165ea3
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-04-14 03:10:42 +0500

    Containing all the necessary Constants Used in the Player

---
+++ player_constants.py
@@ -0,0 +1,12 @@
+
+FILES_FOLDER = CSV_FOLDER = "Files/" # change this to the folder where you want to get your Watched_History.csv from.
+
+
+LOG_PATH = "Logs/Action_Logs.log"
+FAV_PATH = "Files/fav_paths.txt"
+
+
+STYLE_FILE = "../Styles/style.css"
+DEFAULT_FAV= "Files/Favorites.csv"
+
+
