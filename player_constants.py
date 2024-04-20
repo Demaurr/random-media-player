156d3a6b2866319559805b264cd2da361f984be1
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-04-20 07:26:40 +0500

    Changed the Paths to have '\' instead of '/'. Also Added a Folder For Storing Report

--- player_constants.py
+++ player_constants.py
@@ -1,12 +1,13 @@
 
-FILES_FOLDER = CSV_FOLDER = "Files/" # change this to the folder where you want to get your Watched_History.csv from.
+FILES_FOLDER = CSV_FOLDER = "Files\\" # change this to the folder where you want to get your Watched_History.csv from.
+SCREENSHOTS_FOLDER = "Files\\Screenshots\\"
+REPORTS_FOLDER = "Files\\Reports\\"
 
+LOG_PATH = "Logs\\Action_Logs.log"
+FAV_PATH = "Files\\fav_paths.txt"
+WATCHED_HISTORY_LOG_PATH = "Files\\Watched_History.csv"
 
-LOG_PATH = "Logs/Action_Logs.log"
-FAV_PATH = "Files/fav_paths.txt"
-WATCHED_HISTORY_LOG_PATH = "Files/Watched_History.csv"
-
-STYLE_FILE = "../Styles/style.css"
-DEFAULT_FAV= "Files/Favorites.csv"
+STYLE_FILE = "..\\Styles\\style.css"
+DEFAULT_FAV= "Files\\Favorites.csv"
 
 
