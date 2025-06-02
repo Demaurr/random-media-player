0ff7aa3b1d0a4a2ab30c63d5be9c2d1d3bb4ade4
Demaurr <69788671+Demaurr@users.noreply.github.com>
2025-06-02 23:28:31 +0500

    Initial commit of storing default settings

---
+++ default_settings.py
@@ -0,0 +1,25 @@
+"""
+# Path of Folders
+FILES_FOLDER = r"Files"
+CSV_FOLDER = FILES_FOLDER  # change this to the folder where you want to get your Watched_History.csv from.
+SCREENSHOTS_FOLDER = rf"{FILES_FOLDER}\Screenshots"
+REPORTS_FOLDER = rf"{FILES_FOLDER}\Reports"
+LOGS_FOLDER = rf"{FILES_FOLDER}\Logs"
+STYLES_FOLDER = r"Styles"
+DEMO_FOLDER = r"Dummy Data"
+
+# Path of Files
+FOLDER_LOGS = rf"{CSV_FOLDER}\Log_Folders.csv"
+DEMO_WATCHED_HISTORY = rf"{DEMO_FOLDER}\Demo_Watched_History.csv"
+LOG_PATH = rf"{LOGS_FOLDER}\Action_Logs.log"
+FAV_PATH = rf"{FILES_FOLDER}\fav_paths.txt"
+WATCHED_HISTORY_LOG_PATH = rf"{CSV_FOLDER}\Watched_History.csv"
+DELETE_FILES_CSV = rf"{CSV_FOLDER}\To_Delete.csv"
+STYLE_FILE = rf"..\..\{STYLES_FOLDER}\style.css"
+FAV_FILES = rf"{CSV_FOLDER}\Favorites.csv"
+FILE_TRANSFER_LOG = rf"{CSV_FOLDER}\file_transfer_log.csv"
+ALL_MEDIA_CSV = rf"{CSV_FOLDER}\ALL_MEDIA.csv"
+SKIP_FOLDERS = [r"Logs", r"__pycache__", r"venv.bat", r"Styles", r"Files\Reports", r"venv", r"Files\Screenshots"]
+
+CATEGORIES_FILE = rf"{CSV_FOLDER}\categories.csv"
+"""
